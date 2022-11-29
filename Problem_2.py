2c973317c65c48d11da090c9d834292a30d6133f
shreyanshhub <89294432+shreyanshhub@users.noreply.github.com>
2022-11-29 19:02:06 +0530

    create Problem_2.py

---
+++ Problem_2.py
@@ -0,0 +1,17 @@
+def sol():
+  sum = 0
+  
+  dp = [1,1]
+  
+  for i in range(2,4000001):
+    
+    dp[i] = dp[i-1] + dp[i-2]
+    if dp[i] % 2 == 0:
+      sum += dp[i]
+      
+  return sum 
+
+if __name__ == "__main__":
+  sol()
+    
+    
